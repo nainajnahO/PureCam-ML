b463723cef88e0d996f8d451a73d0b963b3c94fc
nainajnahO <d.ohanjanian@icloud.com>
2025-12-26 03:08:06 +0100

    Initial Commit

---
+++ PureCam/PureCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureCamApp.swift
+//  PureCam
+//
+//  Created by David Ohanjanian on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct PureCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
